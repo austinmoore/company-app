0bbd66ac5cf5a8fc2f0bf6840d542436ce7c93a1
Austin Moore <am@shore.com>
2018-03-04 12:28:59 +0100

    Remove generated tests

--- test/system/employees_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class Company::EmployeesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit company_employees_url
-  #
-  #   assert_selector "h1", text: "Company::Employee"
-  # end
-end
